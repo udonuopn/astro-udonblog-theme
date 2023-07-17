7114cae84a5d21ee783be53b05b35bcebc24a22f
udonuopn <miss071125@gmail.com>
2023-07-17 18:02:13 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://udonuopn.github.io',
+  site: 'https://udonuopn.github.io/',
+  base: 'udon-blog'
   integrations: [
     tailwind({
       config: {
