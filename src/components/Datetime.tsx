417051d8dbdefdf7b83e4902b8accadd736d6502
udonuopn <miss071125@gmail.com>
2023-08-13 03:30:22 +0900

    Delete comments

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -20,30 +20,10 @@ const FormattedDatetime = ({ datetime }: { datetime: string | Date }) => {
   const myDatetime = new Date(datetime);
   myDatetime.setTime(myDatetime.getTime() - 1000 * 60 * 60 * 9);
 
-  /*
-  const date = myDatetime.toLocaleDateString(LOCALE, {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-
-  const time = myDatetime.toLocaleTimeString(LOCALE, {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-
-  return (
-    <>
-      {date}
-      <span aria-hidden="true"> | </span>
-      <span className="sr-only">&nbsp;at&nbsp;</span>
-      {time}
-    </>
-  );
-  */
   const day = myDatetime.getDate().toString().padStart(2, "0")
   const year = myDatetime.getFullYear()
   const month = (myDatetime.getMonth() + 1).toString().padStart(2, "0")
+
   return (
     <div className="flex-col font-noto">
       <span className="text-base">{year}-{month}-{day}</span>
